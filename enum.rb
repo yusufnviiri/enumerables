0b24f4dad214202f433c3d1f444c830cc2acef4d
yusufnviiri <yusufnviiri@gmail.com>
2022-07-30 18:08:57 +0300

    Create enumearable list

---
+++ enum.rb
@@ -0,0 +1,26 @@
+require_relative './myEnum'
+
+class MyList
+  include MyEnumerable
+  def initialize(args)
+    @list = args
+  end
+
+  def each(arg)
+    @list.each(arg)
+
+    yield # successive members of @list and uses the MyEnumerable module
+  end
+end
+
+list = MyList.new(1, 2, 3, 4)
+
+p(list.all? { |item| item < 5 })
+
+p(list.all? { |item| item > 5 })
+
+p(list.any? { |item| item == 2 })
+
+p(list.any? { |item| item == 5 })
+
+p(list.filter(&:even?))
--- list.rb
+++ list.rb
@@ -1,20 +1,19 @@
 module MyEnumerable
-    def all?
-        condition = true
-        each { |x| condition = false if !yield(x) }
-        condition
-    end
+  def all?
+    condition = true
+    each { |x| condition = false unless yield(x) }
+    condition
+  end
 
-    def any?
-        condition = false
-        each { |x| condition = true if yield(x) }
-        condition
+  def any?
+    condition = false
+    each { |x| condition = true if yield(x) }
+    condition
+  end
 
-    end
-
-    def filter?
-        list = []
-        each { |x| list << x if yield(x) }
-        list
-    end
+  def filter?
+    list = []
+    each { |x| list << x if yield(x) }
+    list
+  end
 end
