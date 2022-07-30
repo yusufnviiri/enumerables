ee77c9efc1ef495989dfbdd7a7f648ef07f01533
yusufnviiri <yusufnviiri@gmail.com>
2022-07-30 17:19:56 +0300

    Create enumearable list

---
+++ list.rb
@@ -0,0 +1,20 @@
+module MyEnumerable
+    def all?
+        condition = true
+        each { |x| condition = false if !yield(x) }
+        condition
+    end
+
+    def any?
+        condition = false
+        each { |x| condition = true if yield(x) }
+        condition
+
+    end
+
+    def filter?
+        list = []
+        each { |x| list << x if yield(x) }
+        list
+    end
+end
